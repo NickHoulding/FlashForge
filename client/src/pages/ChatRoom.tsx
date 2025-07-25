4cca484310d19908cee04c2891e0988fea3d9e4e
NickHoulding <nih642@gmail.com>
2025-07-24 20:07:31 -0700

    Switching to minimal, simple project structure

--- client/src/pages/ChatRoom.tsx
+++
@@ -1,6 +0,0 @@
-export default function ChatRoom() {
-  return (
-    <div className="container mx-auto px-4 py-8">
-    </div>
-  );
-}
--- client/src/pages/Flashcards.tsx
+++
@@ -1,6 +0,0 @@
-export default function Flashcards() {
-  return (
-    <div className="container mx-auto px-4 py-8">
-    </div>
-  );
-}
