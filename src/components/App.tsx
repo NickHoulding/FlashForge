f30fa9b39f6f2b3278ef1ff25d63948584834df8
NickHoulding <nih642@gmail.com>
2025-07-27 09:52:12 -0700

    Added theme toggle button; Fixed border color transition not applying

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,19 +1,28 @@
 import { Sidebar } from './Sidebar'
 import { Header } from './Header'
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import '../css/sidebar.css'
 import '../css/app.css'
 
 function App() {
   const [isOpen, setIsOpen] = useState(false)
+  const [isDarkTheme, setIsDarkTheme] = useState(true)
 
   const toggleSidebar = () => {
     setIsOpen(!isOpen)
   }
 
+  const toggleTheme = () => {
+    setIsDarkTheme(!isDarkTheme)
+  }
+
+  useEffect(() => {
+    document.documentElement.className = isDarkTheme ? 'dark-theme' : 'light-theme'
+  }, [isDarkTheme])
+
   return (
     <main>
-      <Sidebar isOpen={isOpen} onToggleSidebar={toggleSidebar} />
+      <Sidebar isOpen={isOpen} onToggleSidebar={toggleSidebar} onToggleTheme={toggleTheme} />
       <div className='content'>
         <Header />
         <div className='chat'></div>
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,19 +1,23 @@
 interface SidebarProps {
     isOpen: boolean
     onToggleSidebar: () => void
+    onToggleTheme: () => void
 }
 
-export function Sidebar({ isOpen, onToggleSidebar }: SidebarProps) {
+export function Sidebar({ isOpen, onToggleSidebar, onToggleTheme }: SidebarProps) {
     return (
         <aside className={`sidebar ${isOpen ? 'open' : 'closed'}`}>
-            <div className="controls">
+            <div className="top-controls">
                 <button className="toggle-sidebar" onClick={onToggleSidebar}>
-                    <svg height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
-                        <path fill="currentColor" d="M8,5 L3,5 L3,19 L8,19 L8,5 Z M10,5 L10,19 L21,19 L21,5 L10,5 Z M2.81818182,3 L21.1818182,3 C22.1859723,3 23,3.8954305 23,5 L23,19 C23,20.1045695 22.1859723,21 21.1818182,21 L2.81818182,21 C1.81402773,21 1,20.1045695 1,19 L1,5 C1,3.8954305 1.81402773,3 2.81818182,3 Z"/>
-                    </svg>
+                    <svg fill="currentColor" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"><path d="M4.75 4C3.23122 4 2 5.23122 2 6.75V17.25C2 18.7688 3.23122 20 4.75 20H19.25C20.7688 20 22 18.7688 22 17.25V6.75C22 5.23122 20.7688 4 19.25 4H4.75ZM9 18.5V5.5H19.25C19.9404 5.5 20.5 6.05964 20.5 6.75V17.25C20.5 17.9404 19.9404 18.5 19.25 18.5H9Z"/></svg>
                 </button>
             </div>
             <div id="chats"></div>
+            <div className="bottom-controls">
+                <button className="toggle-theme" onClick={onToggleTheme}>
+                    <svg fill="currentColor" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"><path d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22ZM12 20V4C16.4183 4 20 7.58172 20 12C20 16.4183 16.4183 20 12 20Z"/></svg>
+                </button>
+            </div>
         </aside>
     )
 }
