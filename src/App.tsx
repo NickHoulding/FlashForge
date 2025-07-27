b5bbfb049f9de71f864599d954f4690fa51996a1
NickHoulding <nih642@gmail.com>
2025-07-27 07:30:38 -0700

    Working sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
-import { Header } from './components/Header'
 import { Sidebar } from './components/Sidebar'
-import './css/app.css'
+import { Header } from './components/Header'
+import { useState } from "react";
 import './css/sidebar.css'
+import './css/app.css'
 
 function App() {
+  const [isOpen, setIsOpen] = useState(false)
+
+  const openSidebar = () => {
+      setIsOpen(true)
+  }
+
+  const closeSidebar = () => {
+      setIsOpen(false)
+  }
+
   return (
     <main>
-      <Sidebar />
-      <Header />
+      <Sidebar isOpen={isOpen} onCloseSidebar={closeSidebar} />
+      <Header onOpenSidebar={openSidebar} />
     </main>
   )
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,27 +1,13 @@
-import { useEffect } from "react";
 import { Nav } from "./Nav";
 
-export function Header() {
-    useEffect(() => {
-        const sidebarButton = document.getElementById("sidebar-button")
-        const handleClick = () => {
-            console.log('sidebar toggled...');
-        }
+interface HeaderProps {
+    onOpenSidebar: () => void
+}
 
-        if (sidebarButton) {
-            sidebarButton.addEventListener('click', handleClick);
-        }
-
-        return () => {
-            if (sidebarButton) {
-                sidebarButton.removeEventListener('click', handleClick)
-            }
-        }
-    }, [])
-    
+export function Header({ onOpenSidebar }: HeaderProps) {
     return (
         <header>
-            <button id="sidebar-button">
+            <button className="open-sidebar" onClick={onOpenSidebar}>
                 <svg height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
                     <path fill="currentColor" d="M8,5 L3,5 L3,19 L8,19 L8,5 Z M10,5 L10,19 L21,19 L21,5 L10,5 Z M2.81818182,3 L21.1818182,3 C22.1859723,3 23,3.8954305 23,5 L23,19 C23,20.1045695 22.1859723,21 21.1818182,21 L2.81818182,21 C1.81402773,21 1,20.1045695 1,19 L1,5 C1,3.8954305 1.81402773,3 2.81818182,3 Z"/>
                 </svg>
