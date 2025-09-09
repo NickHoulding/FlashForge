88f064c05916d96fcbcd70c110e2caab85fa3dff
Nicholas Houlding <nicholashoulding@Nicholass-MacBook-Pro.local>
2025-09-09 13:26:46 -0700

    fixed chat and flashcard divider styling

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -35,8 +35,8 @@ const App = () => {
     />
     <div className='relative flex flex-row flex-1 h-screen'>
       <Header onToggleTheme={toggleTheme} />
-      <div className='flex flex-col w-full max-w-[500px] px-[10px] pl-[20px] min-h-0'>
-        <div className='flex flex-col gap-[50px] flex-1 py-[75px] w-full pr-[10px] border-r border-[var(--accent)] mx-auto box-border overflow-auto'>
+      <div className='flex flex-col w-full max-w-[500px] p-[20px] pt-0 pb-[10px] min-h-0'>
+        <div className='flex flex-col gap-[50px] flex-1 py-[75px] w-full mx-auto box-border overflow-auto'>
           {messages.map(message => (
             message.type === 'user' ? (
               <UserQuery 
@@ -48,7 +48,8 @@ const App = () => {
         </div>
         <ChatBox onSendMessage={addMessage} />
       </div>
-      <div className="w-[10px] flex-1 py-[75px] pr-[10px] flex flex-col gap-[10px] overflow-auto">
+      <div className="w-[1px] bg-[var(--secondary)] mt-[75px] mb-[20px] mr-[20px]"></div>
+      <div className="w-[10px] flex-1 py-[75px] pr-[20px] flex flex-col gap-[10px] overflow-auto">
         {messages.map(message => (
           message.type === 'ai' ? (
             <AIResponse 
