71483572d751fee5d07e4d12406a83434651c14f
Nicholas Houlding <nicholashoulding@Nicholass-MacBook-Pro.local>
2025-09-10 16:30:46 -0700

    Installed React Router and implemented proper routing for home and login pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,76 +1,18 @@
-import { useState, useEffect } from "react";
-import type { ChatMessage } from './types';
-import AIResponse from './components/AIResponse';
-import UserQuery from './components/UserQuery';
-import ChatBox from './components/ChatBox';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginPage from "./routes/LoginPage";
-import Sidebar from './components/Sidebar';
-import Header from './components/Header';
-import Modal from './components/Modal';
+import HomePage from "./routes/HomePage";
 import './index.css';
 
 const App = () => {
-  const [messages, setMessages] = useState<ChatMessage[]>([]);
-  const [isDarkTheme, setIsDarkTheme] = useState(true);
-  const [isOpen, setIsOpen] = useState(false);
-  const [isModalOpen, setIsModalOpen] = useState(true);
-  const isLoggedIn = false;
 
-  const toggleSidebar = () => {
-    setIsOpen(!isOpen);
-  };
-
-  const toggleTheme = () => {
-    setIsDarkTheme(!isDarkTheme);
-  };
-
-  const addMessage = (message: ChatMessage) => {
-    setMessages(prev => [...prev, message]);
-  };
-
-  useEffect(() => {
-    document.documentElement.className = isDarkTheme ? 'dark-theme' : 'light-theme'
-  }, [isDarkTheme]);
-
-  if (!isLoggedIn) {
-    return <LoginPage />;
-  }
-  else {
-    return <main className="flex flex-row h-screen">
-      <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)} />
-      <Sidebar
-        isOpen={isOpen}
-        onToggleSidebar={toggleSidebar}
-      />
-      <div className='relative flex flex-row flex-1 h-screen'>
-        <Header onToggleTheme={toggleTheme} />
-        <div className='flex flex-col w-full max-w-[500px] p-[20px] pt-0 pb-[10px] min-h-0'>
-          <div className='flex flex-col gap-[50px] flex-1 py-[75px] w-full mx-auto box-border overflow-auto'>
-            {messages.map(message => (
-              message.type === 'user' ? (
-                <UserQuery 
-                  key={message.id} 
-                  content={message.content}
-                />
-              ) : null
-            ))}
-          </div>
-          <ChatBox onSendMessage={addMessage} />
-        </div>
-        <div className="w-[1px] bg-[var(--secondary)] mt-[75px] mb-[20px] mr-[20px]"></div>
-        <div className="w-[10px] flex-1 py-[75px] pr-[20px] flex flex-col gap-[10px] overflow-auto">
-          {messages.map(message => (
-            message.type === 'ai' ? (
-              <AIResponse 
-                key={message.id} 
-                flashcards={message.flashcards || []}
-              />
-            ) : null
-          ))}
-        </div>
-      </div>
-  </main>;
-  }
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
